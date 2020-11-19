e47cf844c6578d9dc48ae07cf6d09a02ab49d7f3
Vineet Sridhar <vineets1600@gmail.com>
2020-11-18 22:58:12 -0500

    Registered parse subclass

--- app/src/main/java/com/example/instagramclone/MainActivity.java
+++ app/src/main/java/com/example/instagramclone/MainActivity.java
@@ -2,12 +2,21 @@ package com.example.instagramclone;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnSubmit, btnCaptureImage;
+    EditText etDescription;
+    ImageView ivImage;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
 }
--- app/src/main/java/com/example/instagramclone/ParseApplication.java
+++ app/src/main/java/com/example/instagramclone/ParseApplication.java
@@ -3,11 +3,13 @@ package com.example.instagramclone;
 import android.app.Application;
 
 import com.parse.Parse;
+import com.parse.ParseObject;
 
 public class ParseApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        ParseObject.registerSubclass(Post.class);
         Parse.initialize(new Parse.Configuration.Builder(this)
                 .applicationId("PMf6R7tiKOTtVfmwXjFr7uYrW04CUGosd6aHNPon")
                 .clientKey("nMaPyUEhVNO1x1XsvI4j7r79WGE4bMvPerUSbf04")
