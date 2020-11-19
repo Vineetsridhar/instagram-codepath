4132f1d9eb621e7e2014c30d7082f2afcc9f6e20
Vineet Sridhar <vineets1600@gmail.com>
2020-11-18 22:31:09 -0500

    Added base application features

---
+++ app/src/main/java/com/example/instagramclone/LoginActivity.java
@@ -0,0 +1,34 @@
+package com.example.instagramclone;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class LoginActivity extends AppCompatActivity {
+    Button btnLogin;
+    EditText etUsername, etPassword;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        etUsername = findViewById(R.id.etUsername);
+        etPassword = findViewById(R.id.etPassword);
+        btnLogin = findViewById(R.id.btLogin);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = etUsername.getText().toString();
+                String password = etPassword.getText().toString();
+                loginUser(username, password);
+            }
+        });
+    }
+
+    private void loginUser(String username, String password){
+
+    }
+}
